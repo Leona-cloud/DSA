23f98fa2f8cf7886acc9d9b62501faef4ccb4b9e
Leona-cloud <chisomokafor038@gmail.com>
2023-08-03 18:39:15 +0100

    chore: daily tempratures question

---
+++ Stack/dailyTemprature.js
@@ -0,0 +1,26 @@
+// Given an array of integers temperatures represents the daily temperatures,
+//return an array answer such that answer[i] is the number of days you have to wait after the ith
+//day to get a warmer temperature. If there is no future day for which this is possible,
+//keep answer[i] == 0 instead.
+
+function dailyTemp(temperature) {
+  let n = temperature.length;
+  let res = new Array(n).fill(0);
+  let stack = [];
+
+  for (let i = 0; i < n; i++) {
+    while (stack.length && temperature[i] > temperature[stack[stack.length - 1]]) {
+        console.log(temperature[stack[stack.length - 1]])
+      let index = stack.pop();
+      res[index] = i - index;
+    } 
+    stack.push(i);
+  }
+  return res;
+}
+
+
+
+const temperature = [73,74,75,71,69,72,76,73];
+var newNum = dailyTemp(temperature);
+console.log(newNum);
