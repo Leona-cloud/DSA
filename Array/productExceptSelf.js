46a274075ff3ded0a4ab479b43bd90e4b418cf3f
Leona-cloud <chisomokafor038@gmail.com>
2023-08-10 09:50:42 +0100

    chore : more array questions

---
+++ Array/productExceptSelf.js
@@ -0,0 +1,33 @@
+// Given an integer array nums, return an array answer such that answer[i] is equal 
+// to the product of all the elements of nums except nums[i].
+
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+// You must write an algorithm that runs in O(n) time and without using the division operation.
+
+
+
+
+var productExceptSelf = function(nums) {
+
+    let res = new Array(nums.length).fill(1);
+    let prefix = 1;
+
+    for (let i = 0; i<nums.length; i++){
+        res[i] = prefix
+        console.log(res[i])
+        prefix *= nums[i]
+    }
+   let postfix = 1
+   for(let i = nums.length-1; i>=0; i--){
+        res[i] *= postfix
+        postfix *= nums[i]
+   };
+
+   return res
+    
+};
+
+
+const nums =  [1,2,3,4]
+console.log(productExceptSelf(nums))
