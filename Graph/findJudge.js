7af7adc95f40f9470d1e3aefabb6d78a2d20508e
Leona-cloud <chisomokafor038@gmail.com>
2023-08-13 18:28:31 +0100

    chore: graph question

---
+++ Graph/findJudge.js
@@ -0,0 +1,35 @@
+
+
+var findJudge = function(n, trust) {
+  
+    const trusts = new Array(n + 1).fill(0); // Index 0 is not used
+    const trustedBy = new Array(n + 1).fill(0);
+
+    for([a, b] of trust){
+        trustedBy[a]++;
+        trusts[b]++;
+
+    };
+    console.log(trusts, trustedBy)
+
+    //to find the town judge
+    for(let i = 1; i<=n; i++){
+        //if judge trusts no one and everyone trusts the judge
+        if(trusts[i] === n-1 && trustedBy[i] === 0){
+            return i
+        }
+    }
+    
+    return -1
+
+};
+
+
+
+
+
+const n = 3; 
+const trust = [[1,3],[2,3]]
+
+
+console.log(findJudge(n, trust))
